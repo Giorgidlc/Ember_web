3d6548981d09d41ecd8e69816d24dc20924361bf
Giorgi Visual <giorgivisual@gmail.com>
2025-09-06 01:25:24 +0200

    Add index(contect with wp) and Assets

--- wp-content/themes/emberTheme/emberFrontendWeb/src/libs/wp.ts
+++ wp-content/themes/emberTheme/emberFrontendWeb/src/libs/wp.ts
@@ -6,30 +6,60 @@ interface PropsPages {
   slug: string;
   page: string;
   title: { rendered: string };
-  content: { rendered: string };
+  acf: PropsAcf;
   excerpt: { rendered: string };
   _embedded: any;
 }
 
-export const pagesInfo = async (slug: PropsPages) => {
+interface PropsAcf {
+  title: string;
+  description: string;
+  btn_url: string;
+  intro_title: string;
+  intro_description: string;
+
+}
+
+interface PropsServices {
+  id: number;
+  title: { rendered: string };
+  excerpt: { rendered: string };
+  slug: string;
+  acf: {
+    description_service: string;
+    button_service: {
+      title: string;
+      url: string;
+      target: string;
+    };
+  };
+  _embedded: any;
+}
+
+export const pagesInfo = async (slug: string) => {
   
   try {
   const res = await fetch(`${apiUrl}/pages?slug=${slug}&_embed`);
   if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
     
-  const data = await res.json();
+  const data: PropsPages[] = await res.json();
   if (data.length === 0) throw new Error("No page found with the given slug.");
   
     const [infoPage] = data.map((page: PropsPages) => {
       const {
       slug: pageSlug,
-      title: { rendered: title },
-      content: { rendered: content },
+      title: { rendered: titlePage },
+      acf: {
+        title: acfTitle,
+        description: acfDescription,
+        btn_url: btnUrl,
+        intro_title: introTitle,
+      },
       excerpt: { rendered: excerpt },
       _embedded,
       } = page;
 
-      return { pageSlug, title , content, excerpt, _embedded }
+      return { pageSlug, titlePage, title: acfTitle, acfDescription, btnUrl, introTitle, excerpt, _embedded }
     })
     console.log(infoPage)
     return infoPage;
@@ -40,6 +70,59 @@ export const pagesInfo = async (slug: PropsPages) => {
   }
 }
 
+
+export const getAllServices = async ({ perPage = 4 }: { perPage?: number } = {}) => {
+  try {
+    const res = await fetch(`${apiUrl}/servicio?per_page=${perPage}&_embed`);
+    if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+
+    const results = await res.json();
+    if (results.length === 0) throw new Error("No services found.");
+
+    const services = results.map((service:PropsServices) => {
+      const {
+        id: serviceId,
+        title: { rendered: titleService },
+        excerpt: { rendered: excerpt },
+        slug: slug,
+        acf: { 
+          description_service: descriptionService,
+          button_service: { 
+            title: buttonText,
+            url: urlService,
+            target: targetService
+          } = { title: "Ver más", url: "", target: "" }
+        } = { description_service: "", button_service:{} },
+       /*  _embedded */
+      } = service;
+    
+     /*  // Manejo seguro de imagen destacada
+      const srcLink = _embedded?.["wp:featuredmedia"]?.[0]?.source_url || ""; */
+      
+      return { 
+        id: serviceId, 
+        title: titleService, 
+        description: descriptionService,
+        excerpt, 
+        buttonText,
+        urlService,
+        targetService
+        /*  srcLink,
+        slug, 
+        buttonText, 
+        urlService, 
+        targetService */
+      };
+    }
+    )
+    console.log(services)
+    return services;
+
+  } catch (error) {
+    console.error("Error fetching services:", error);
+    throw error;
+  }
+} 
 
 export const getAllPosts = async ({ perPage = 3 }: { perPage?: number } = {}) => { 
   try {
